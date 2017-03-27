9ef581c603ee79abd2d1ae99623a8ea52d687c6d
msanzau <markelsanz14@gmail.com>
2017-03-27 11:54:06 -0400

    updates

--- convolutionalNN.py
+++ convolutionalNN.py
@@ -1,26 +1,41 @@
 import tensorflow as tf
+import numpy as np
 
 def CNN(features, labels):
 	
 	sess = tf.InteractiveSession()
 
+	# Placeholders for our training features and labels
 	x = tf.placeholder(tf.float32, shape=[None, 65536])
 	y_ = tf.placeholder(tf.float32, shape=[None, 8])
 	
+	# Weights and Biases for our learning algorithm
 	W = tf.Variable(tf.zeros([65536,8]))
 	b = tf.Variable(tf.zeros([8]))
 
 	print("VARIABLES INITIALIZED")
 
+	#Initialize variables
 	sess.run(tf.global_variables_initializer())
 
+	# Linear Regression function
 	y = tf.matmul(x,W) + b
+	# Loss function
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
-	train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+	# Training step (gradient descent)
+	#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+	np.reshape(features, (len(features), 65536))
+	#print(tf.shape(features))
+	print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	#print(tf.shape(labels))
 
 	print("TRAINING STARTED")
+	# Training: 1000 iterations
 	for step in range(1000):
+		#Select a new batch
 		batch = tf.train.batch([features, labels], batch_size=100)
+		#Run one more step of gradient descent
 		train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 		#if (step % 50 == 0):
             #print('Loss at step %d: %f' % (step, l))
@@ -30,4 +45,3 @@ def CNN(features, labels):
 	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 	print(accuracy.eval(feed_dict={x: features, y_: labels}))
 	
-
