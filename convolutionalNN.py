230194704aaadad865338b771772ad44c02c7a37
msanzau <markelsanz14@gmail.com>
2017-03-29 12:48:12 -0400

    Convolutional Neural Network initial iteration working

--- convolutionalNN.py
+++ convolutionalNN.py
@@ -1,17 +1,23 @@
 import tensorflow as tf
 import numpy as np
+import random
 
 def CNN(features, labels):
 	
 	sess = tf.InteractiveSession()
 
+	group = list(zip(features, labels))
+	batch_size = 100
 	# Placeholders for our training features and labels
-	x = tf.placeholder(tf.float32, shape=[None, 65536])
-	y_ = tf.placeholder(tf.float32, shape=[None, 8])
+	x = tf.placeholder(tf.float32, shape=[batch_size, 65536]) #3 for RGB
+	# REAL LABELS
+	num_labels = 8
+	y_ = tf.placeholder(tf.float32, shape=[batch_size])
+	#labels = (np.arange(num_labels) == labels[:]).astype(np.float32)
 	
 	# Weights and Biases for our learning algorithm
-	W = tf.Variable(tf.zeros([65536,8]))
-	b = tf.Variable(tf.zeros([8]))
+	W = tf.Variable(tf.zeros([65536, 1]))
+	b = tf.Variable(tf.zeros([1]))
 
 	print("VARIABLES INITIALIZED")
 
@@ -19,29 +25,30 @@ def CNN(features, labels):
 	sess.run(tf.global_variables_initializer())
 
 	# Linear Regression function
-	y = tf.matmul(x,W) + b
+	y = tf.transpose(tf.matmul(x, W) + b)
 	# Loss function
 	cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 	# Training step (gradient descent)
-	#train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
-
-	np.reshape(features, (len(features), 65536))
-	#print(tf.shape(features))
-	print("AAAAAAAAAAAAAAAAAAAAAAAAAAA")
-	#print(tf.shape(labels))
+	train_step = tf.train.GradientDescentOptimizer(0.05).minimize(cross_entropy)
 
 	print("TRAINING STARTED")
 	# Training: 1000 iterations
-	for step in range(1000):
+	for step in range(10):
 		#Select a new batch
-		batch = tf.train.batch([features, labels], batch_size=100)
+		batch = random.sample(group, batch_size)
+		unzipped_batch = list(zip(*batch))
+		batch_x = unzipped_batch[0]
+		batch_y = unzipped_batch[1]
+		#Train on batch
+		train_data = {x : batch_x, y_: batch_y}
 		#Run one more step of gradient descent
-		train_step.run(feed_dict={x: batch[0], y_: batch[1]})
+		train_step.run(feed_dict=train_data)
+		print(step)
 		#if (step % 50 == 0):
             #print('Loss at step %d: %f' % (step, l))
             #print('Training accuracy: %.1f%%' % accuracy(predictions, tf_train_labels))
 
-	correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-	accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-	print(accuracy.eval(feed_dict={x: features, y_: labels}))
+	#correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+	#accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+	#print(accuracy.eval(feed_dict={x: features, y_: labels}))
 	
--- readImages.py
+++ readImages.py
@@ -1,6 +1,7 @@
 from PIL import Image
 import os
 import numpy as np
+import pandas as pd
 
 #Reads images from directory and creates the training set
 def readImages(directory):
@@ -13,10 +14,9 @@ def readImages(directory):
 		#Read label from image (label = first character of image name)
 		label = int(im[0])
 		#Read image pixels and store them as an array
-		image_pixels = np.asarray(image.getdata()).reshape((image.size[1]*image.size[1], 1))
+		image_pixels = np.asarray(image.getdata()).reshape((image.size[1]*image.size[1]))
 		#Add pixels as features to feature list
 		features_train.append(image_pixels)
 		#Add label to label list
 		labels_train.append(label)
-
 	return features_train, labels_train
