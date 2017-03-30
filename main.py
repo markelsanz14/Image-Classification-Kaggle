1b7d234f646a0c45a8b1232c5fa6e0d5079ad0cd
msanzau <markelsanz14@gmail.com>
2017-03-29 23:30:39 -0400

    Regression for classification done. 82% accuracy achieved on test set

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import readImages as ri
 import convolutionalNN as cnn
+import regression as reg
 
 #
 def main():
 	path_to_images = '../NewImages/'
-	features_train, labels_train = ri.readImages(path_to_images)
+	features_train, labels_train, features_val, labels_val = ri.readImages(path_to_images)
 	print("TRAINING SET READ")
-	cnn.CNN(features_train, labels_train)
+	reg.CNN(features_train, labels_train, features_val, labels_val)
 
 if __name__ == "__main__":
 	main()
--- readImages.py
+++ readImages.py
@@ -7,16 +7,23 @@ import pandas as pd
 def readImages(directory):
 	features_train = []
 	labels_train = []
-	
+	ident = np.identity(8)
+
 	#Directory to read the images from
 	for im in os.listdir(directory):
 		image = Image.open(directory+im)
 		#Read label from image (label = first character of image name)
 		label = int(im[0])
+		label_one_hot = ident[label]
 		#Read image pixels and store them as an array
 		image_pixels = np.asarray(image.getdata()).reshape((image.size[1]*image.size[1]))
 		#Add pixels as features to feature list
 		features_train.append(image_pixels)
 		#Add label to label list
-		labels_train.append(label)
-	return features_train, labels_train
+		labels_train.append(label_one_hot)
+
+	features_val = features_train[:3000]
+	features_train = features_train[3000:]
+	labels_val = labels_train[:3000]
+	labels_train = labels_train[3000:]
+	return features_train, labels_train, features_val, labels_val
